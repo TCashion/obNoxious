20d63020ee28528c6c126cfe4b7fbccce68b3508
TCashion <58757351+TCashion@users.noreply.github.com>
2020-07-15 15:06:46 -0600

    parse distribution and taxonomy in verification form

--- src/pages/Plants/AddPlant/AddPlant.jsx
+++ src/pages/Plants/AddPlant/AddPlant.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as natureserveAPI from '../../../services/natureserveAPI';
+import './AddPlant.css';
 
 const style = {
     marginTop: '20vh',
@@ -64,7 +65,7 @@ class AddPlant extends Component {
         }));
     }
 
-    handleSubmit = async (e) => {
+    handleSubmitSearch = async (e) => {
         e.preventDefault();
         try {
             const newPlant = await this.getNatureServePlant(this.state.plant.commonName);
@@ -75,6 +76,23 @@ class AddPlant extends Component {
         } catch (err) {
             this.updateMessage('Error - try another search term.')
         }
+    }
+
+    parseDistribution = () => {
+        let distribution = this.state.plant.distribution.map(contaminatedState => contaminatedState);
+        distribution.sort();
+        return distribution.join(', ')
+    }
+
+
+    parseTaxonomy = () => {
+        let taxonomy = [];
+        for (const key in this.state.plant.taxonomy) {
+            const upperKey = key[0].toUpperCase() + key.substr(1);
+            const pairString = `${upperKey}: ${this.state.plant.taxonomy[key]}`
+            taxonomy.push(pairString)
+        };
+        return taxonomy.join(' > ');
     }
 
     updateMessage = (msg) => {
@@ -103,19 +121,56 @@ class AddPlant extends Component {
                     <div className="col s12 m6">
                         <div className="card">
                             <div className="card-content">
-                                <div className="card-title">
-                                    Enter the common name for the plant you wish to add:
-                            </div>
-                                <div>
-                                    <form onSubmit={this.handleSubmit} >
-                                        <div className="col-sm-12">
-                                            <input type="text" className="form-control" placeholder="Common Name" value={this.state.plant.commonName} name="commonName" onChange={this.handleChange} />
+                                {this.state.plant.nsxUrl ?
+                                    <>
+                                        <div className="card-title">
+                                            Is this the plant you wish to add to the database?:
                                         </div>
-                                        <div className="col-sm-12 text-center">
-                                            <button className="btn btn-default" disabled={this.validateForm()}>Search</button> the <a href="https://explorer.natureserve.org/" target="_blank" rel="noopener noreferrer">NatureServeExplorer</a> Database&nbsp;&nbsp;&nbsp;
-                                    </div>
-                                    </form>
-                                </div>
+                                        <div>
+                                            <form >
+                                                <div className="input-field col s12">
+                                                    <label for="commonName" className="active">Common Name:</label>
+                                                    <input type="text" className="validate" value={this.state.plant.commonName} name="commonName" disabled />
+                                                </div>
+                                                <div className="input-field col s12">
+                                                    <label for="scientificName" className="active">Scientific Name:</label>
+                                                    <input type="text" className="validate" value={this.state.plant.scientificName} name="scientificName" disabled />
+                                                </div>
+                                                <div className="input-field col s12">
+                                                    <label for="taxonomy" className="active">Taxonomy:</label>
+                                                    <textarea name="taxonomy" className="materialize-textarea" id="taxonomy" cols="30" rows="10" disabled>{this.parseTaxonomy()}</textarea>
+                                                </div>
+                                                <div className="input-field col s12">
+                                                    <label for="distribution" className="active">Distribution:</label>
+                                                    <textarea name="distribution" className="materialize-textarea" id="distribution" cols="30" rows="10" disabled>{this.parseDistribution()}</textarea>
+                                                </div>
+                                                <div className="col-sm-12 text-center">
+                                                    <button className="btn btn-default">Yes</button> 
+                                                    <button className="btn btn-danger">No</button> 
+                                                </div>
+                                                <div className="col-sm-12 text-center">
+                                                    <p>Data provided by: <a href="https://explorer.natureserve.org/" target="_blank" rel="noopener noreferrer">NatureServeExplorer</a></p>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </>
+                                    :
+                                    <>
+                                        <div className="card-title">
+                                            Enter the common name for the plant you wish to add:
+                                        </div>
+                                        <div>
+                                            <form onSubmit={this.handleSubmitSearch} >
+                                                <div className="col-sm-12">
+                                                    <input type="text" className="form-control" placeholder="Common Name" value={this.state.plant.commonName} name="commonName" onChange={this.handleChange} />
+                                                </div>
+                                                <div className="col-sm-12 text-center">
+                                                    <button className="btn btn-default" disabled={this.validateForm()}>Search</button> the <a href="https://explorer.natureserve.org/" target="_blank" rel="noopener noreferrer">NatureServeExplorer</a> Database&nbsp;&nbsp;&nbsp;
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </>
+                                }
                             </div>
                         </div>
                     </div>
