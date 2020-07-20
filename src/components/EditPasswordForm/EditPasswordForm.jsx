6c065739f16611263f2563eb98672c72f69963cd
TCashion <58757351+TCashion@users.noreply.github.com>
2020-07-20 16:04:20 -0600

    add message logic to edit password form

--- src/components/EditPasswordForm/EditPasswordForm.jsx
+++ src/components/EditPasswordForm/EditPasswordForm.jsx
@@ -3,32 +3,51 @@ import userService from '../../services/userService';
 
 class EditPasswordForm extends Component {
     state = {
-        email: this.props.user.email,
-        password: '',
-        newPassword: '',
-        passwordConf: ''
+        formData: {...this.getInitialFormData()}
+    }
+    
+    getInitialFormData() {
+        return {
+            email: this.props.user.email,
+            password: '',
+            newPassword: '',
+            passwordConf: '',
+            message: ''
+        }
     }
 
     handleChange = (e) => {
-        this.props.updateMessage('');
-        this.setState({
-            [e.target.name]: e.target.value,
-        })
+        e.persist()
+        this.updateMessage('');
+        this.setState((state) => ({
+            formData: {
+                ...this.state.formData,
+                [e.target.name]: e.target.value
+            }
+        }))
     }
 
     handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await userService.updatePassword(this.state);
-            this.props.updateMessage('Password change successful.');
-            this.props.history.push('/');
+            await userService.updatePassword(this.state.formData);
+            this.updateMessage('Password change successful.');
+            this.setState({
+                formData: {...this.getInitialFormData()}
+            })
         } catch (err) {
-            this.props.updateMessage(err.message)
+            this.updateMessage(err.message)
         }
     }
 
+    updateMessage = (msg) => {
+        this.setState({
+            message: msg,
+        });
+    }
+
     validateForm() {
-        return !(this.state.password && this.state.newPassword === this.state.passwordConf);
+        return !(this.state.formData.password && this.state.formData.newPassword === this.state.formData.passwordConf);
     }
 
     render() {
@@ -44,18 +63,22 @@ class EditPasswordForm extends Component {
                                 <form className="user-form" onSubmit={this.handleSubmit}>
                                     <div className="input-field col-sm-12">
                                         <label htmlFor="password" className="active">Current password: </label>
-                                        <input type="password" name="password" minLength="6" value={this.state.existing} onChange={this.handleChange} />
+                                        <input type="password" name="password" minLength="6" value={this.state.formData.existing} onChange={this.handleChange} />
                                     </div>
                                     <div className="input-field col-sm-12">
                                         <label htmlFor="newPassword" className="active">New password: </label>
-                                        <input type="password" name="newPassword" minLength="6" value={this.state.newPassword} onChange={this.handleChange} />
+                                        <input type="password" name="newPassword" minLength="6" value={this.state.formData.newPassword} onChange={this.handleChange} />
                                     </div>
                                     <div className="input-field col-sm-12">
                                         <label htmlFor="passwordConf" className="active">Confirm new password: </label>
-                                        <input type="password" name="passwordConf" minLength="6" value={this.state.passwordConf} onChange={this.handleChange} />
+                                        <input type="password" name="passwordConf" minLength="6" value={this.state.formData.passwordConf} onChange={this.handleChange} />
                                     </div>
                                     <div className="col-sm-12">
-                                        <button className="btn btn-default" type="submit" disabled={this.validateForm()}>UPDATE</button>
+                                        {this.state.message ? 
+                                            <p style={{ color: 'green' }}>{this.state.message}</p>
+                                            :
+                                            <button className="btn btn-default" type="submit" disabled={this.validateForm()}>UPDATE</button>
+                                        }
                                     </div>
                                 </form>
                             </div>
